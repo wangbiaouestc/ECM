d2bb16f80fb7f7b0d0f6e583dd9fc0f603cd8c59
Roman Chernyak <chernyak@global.tencent.com>
2023-07-24 16:35:29 -0700

    JVET-AE0180: Add missing condition on PIC_RECON_WRAP buffer allocation

--- source/Lib/CommonLib/Picture.h
+++ source/Lib/CommonLib/Picture.h
@@ -99,11 +99,14 @@ struct Picture : public UnitArea
   uint32_t margin;
   Picture();
 
+void create(
 #if JVET_Z0118_GDR
-  void create(const bool gdrEnabled, const ChromaFormat &_chromaFormat, const Size &size, const unsigned _maxCUSize, const unsigned _margin, const bool _decoder, const int _layerId, const bool gopBasedTemporalFilterEnabled = false);
-#else
-  void create( const ChromaFormat &_chromaFormat, const Size &size, const unsigned _maxCUSize, const unsigned margin, const bool bDecoder, const int layerId, const bool gopBasedTemporalFilterEnabled = false );
+    const bool gdrEnabled,
 #endif
+    const bool useWrapAround, const ChromaFormat &_chromaFormat, const Size &size, const unsigned _maxCUSize,
+    const unsigned _margin,
+    const bool _decoder, const int _layerId, const bool gopBasedTemporalFilterEnabled = false);
+
   void destroy();
 
   void createTempBuffers( const unsigned _maxCUSize );
