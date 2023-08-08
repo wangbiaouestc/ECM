c1372e8c8e4378e32219620b560fb3723bdbfb0a
Hongtao Wang <hongtaow@qti.qualcomm.com>
2023-08-08 22:17:08 +0000

    Fix: sps_bvg_cccm signaling when --EnableTMnoninterTools=0

--- source/Lib/EncoderLib/VLCWriter.cpp
+++ source/Lib/EncoderLib/VLCWriter.cpp
@@ -1656,6 +1656,9 @@ void HLSWriter::codeSPS( const SPS* pcSPS )
 #if JVET_AC0147_CCCM_NO_SUBSAMPLING
     WRITE_UVLC(pcSPS->getUseCccm(), "sps_cccm_cand");
 #endif
+#if JVET_AE0100_BVGCCCM
+    WRITE_FLAG(pcSPS->getUseBvgCccm() ? 1 : 0, "sps_bvg_cccm");
+#endif
   }
 #endif
   if( pcSPS->getChromaFormatIdc() != CHROMA_400)
