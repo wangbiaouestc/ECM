21a21652f2469199d5b3cf8aa1cefa525606ebbc
Yang Wang <wangyang.cs@bytedance.com>
2024-08-24 08:45:50 +0800

    Set GeoInerIbc in config file explicitly to enable it for SCC

--- source/App/EncoderApp/EncAppCfg.cpp
+++ source/App/EncoderApp/EncAppCfg.cpp
@@ -1198,7 +1198,7 @@ bool EncAppCfg::parseCfg( int argc, char* argv[] )
 #endif
   ("Geo",                                             m_Geo,                                            false, "Enable geometric partitioning mode (0:off, 1:on)")
 #if JVET_AI0082_GPM_WITH_INTER_IBC
-  ("GeoInterIbc",                                     m_geoInterIbc,                                    true, "GPM with inter and IBC (0:off, 1:on)  [default: off]" )
+  ("GeoInterIbc",                                     m_geoInterIbc,                                    false, "GPM with inter and IBC (0:off, 1:on)  [default: off]" )
 #endif
   ("HashME",                                          m_HashME,                                         false, "Enable hash motion estimation (0:off, 1:on)")
 
